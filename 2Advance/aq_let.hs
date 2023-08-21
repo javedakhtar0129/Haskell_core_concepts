1da6a8fb8c8164c450bc9e22359cf401c5377636
JAVED AKHTAR <javedakhtar@JAVEDs-MacBook-Air.local>
2023-08-21 21:15:09 +0530

    let in ghci

---
+++ 2Advance/aq_let.hs
@@ -0,0 +1,7 @@
+-- At a basic level let and where seems similar
+-- Prelude> let sqr x = x*x for on-the-fly declarations
+-- use :{, :} for multiline definitions
+-- we use let in the ghci to declare a function
+-- So, the fact that we cannot directly define functions in ghci like python is not entirely accurate
+
+
