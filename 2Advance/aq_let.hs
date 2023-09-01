75d5f6abb5a89ce6a9dc338968e08e445936aa0f
JAVED AKHTAR <javedakhtar@JAVEDs-MacBook-Air.local>
2023-09-01 23:01:44 +0530

    let ghci

--- 2Advance/aq_let.hs
+++ 2Advance/aq_let.hs
@@ -3,5 +3,3 @@
 -- use :{, :} for multiline definitions
 -- we use let in the ghci to declare a function
 -- So, the fact that we cannot directly define functions in ghci like python is not entirely accurate
-
-
